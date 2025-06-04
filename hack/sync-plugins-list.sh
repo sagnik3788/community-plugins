adb6ca9184ddc16d849afa625df97737f977e5b4
t-kikuc <tkikuchi07f@gmail.com>
2025-06-04 18:40:21 +0900

    add labeler (pr)
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>

--- hack/sync-plugins-list.sh
+++ hack/sync-plugins-list.sh
@@ -2,18 +2,33 @@
 # This script updates the plugins list in the issue templates.
 
 plugins=$(ls plugins | sed 's/\// /g')
-files=".github/ISSUE_TEMPLATE/bug-report.yaml .github/ISSUE_TEMPLATE/enhancement.yaml"
+issue_templates=".github/ISSUE_TEMPLATE/bug-report.yaml .github/ISSUE_TEMPLATE/enhancement.yaml"
 
+# 1. Issue Templates
 plugins_list=""
-
 for plugin in $plugins; do
     plugins_list="${plugins_list}        - $plugin\\
 "
 done
 
-for f in $files; do
+for f in $issue_templates; do
     sed -i '' "/# --- Start plugins list ---/,/# --- End plugins list ---/c\\
         # --- Start plugins list ---\\
 $plugins_list\\        # --- End plugins list ---\\
 " "$f"
 done
+
+# 2. labeler
+labeler_config_file=".github/labeler.yaml"
+plugin_labels=""
+for plugin in $plugins; do
+    plugin_labels="${plugin_labels}\\
+plugin/$plugin: \\
+  - changed-files: \\
+      - any-glob-to-any-file: "plugins/$plugin/**"\\
+"
+done
+sed -i '' "/# --- Start plugins list ---/,/# --- End plugins list ---/c\\
+# --- Start plugins list ---\\
+$plugin_labels\\# --- End plugins list ---\\
+" "$labeler_config_file"
